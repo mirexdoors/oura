ea7ae76763643a37b59d6321cbf6bea584e08308
mirexdoors <mirex.doors@mail.ru>
2020-05-26 04:07:47 +0700

    -- add multiple date-pickers in mean

--- src/components/controls/ControlsMean.vue
+++ src/components/controls/ControlsMean.vue
@@ -1,48 +1,110 @@
 <template>
     <div class="pl-2">
         <div class="subtitle-2">{{text}}</div>
-        <v-menu
-                ref="menu1"
-                v-model="menu1"
-                :close-on-content-click="false"
-                transition="scale-transition"
-                offset-y
-                max-width="290px"
-                min-width="290px"
-        >
-            <template v-slot:activator="{ on }">
-                <v-text-field
-                        v-model="date1"
-                        label="Date Start"
-                        persistent-hint
-                        v-on="on"
-                ></v-text-field>
-            </template>
-            <v-date-picker @change="changeInput" v-model="date1" :allowed-dates="allowedDatesStart"  no-title
-                           @input="menu1 = false"></v-date-picker>
-        </v-menu>
-        <v-menu
-                v-model="menu2"
-                :close-on-content-click="false"
-                transition="scale-transition"
-                offset-y
-                max-width="290px"
-                min-width="290px"
-        >
-            <template v-slot:activator="{ on }">
-                <v-text-field
-                        v-model="date2"
-                        label="Date End"
-                        persistent-hint
-                        readonly
-                        v-on="on"
-                ></v-text-field>
-            </template>
-            <v-date-picker v-model="date2"  :allowed-dates="allowedDatesFinish" @change="changeInput" no-title
-                           @input="menu2 = false"></v-date-picker>
-        </v-menu>
+        <v-switch
+                :label="switchType ? 'Custom (under construction)' : 'Range'"
+                v-model="switchType"
+        ></v-switch>
+        <div v-if="!switchType">
+            <v-menu
+                    ref="menu1"
+                    v-model="menu1"
+                    :close-on-content-click="false"
+                    transition="scale-transition"
+                    offset-y
+                    max-width="290px"
+                    min-width="290px"
+            >
+                <template v-slot:activator="{ on }">
+                    <v-text-field
+                            v-model="date1"
+                            label="Date Start"
+                            persistent-hint
+                            v-on="on"
+                    ></v-text-field>
+                </template>
+                <v-date-picker @change="changeInput" v-model="date1" :allowed-dates="allowedDatesStart" no-title
+                               @input="menu1 = false"></v-date-picker>
+            </v-menu>
+            <v-menu
+                    v-model="menu2"
+                    :close-on-content-click="false"
+                    transition="scale-transition"
+                    offset-y
+                    max-width="290px"
+                    min-width="290px"
+            >
+                <template v-slot:activator="{ on }">
+                    <v-text-field
+                            v-model="date2"
+                            label="Date End"
+                            persistent-hint
+                            readonly
+                            v-on="on"
+                    ></v-text-field>
+                </template>
+                <v-date-picker v-model="date2" :allowed-dates="allowedDatesFinish" @change="changeInput" no-title
+                               @input="menu2 = false"></v-date-picker>
+            </v-menu>
+        </div>
+        <div v-else>
+<!--            <v-dialog-->
+<!--                    ref="dialog"-->
+<!--                    v-for="(menu, index) in customMenu"-->
+<!--                    v-model="menu.isActive"-->
+<!--                    :key="menu.input"-->
+<!--                    :return-value.sync="menu.isActive"-->
+<!--                    persistent-->
+<!--                    width="290px"-->
+<!--            >-->
+<!--                <template v-slot:activator="{ on }">-->
+<!--                    <v-text-field-->
+<!--                            v-model="menu.input"-->
+<!--                            label="Date"-->
+<!--                            prepend-icon="event"-->
+<!--                            :append-outer-icon="(index === customMenu.length - 1) ? 'mdi-plus-thick' :-->
+<!--                            'mdi-close-circle-outline'"-->
+<!--                            readonly-->
+<!--                            @click:append-outer="manageInput(index)"-->
+<!--                            v-on="on"-->
+<!--                    ></v-text-field>-->
+<!--                </template>-->
+<!--                <v-date-picker v-model="menu.input" scrollable range>-->
+<!--                    <v-spacer></v-spacer>-->
+<!--                    <v-btn text color="primary" @click="menu.isActive = false">Cancel</v-btn>-->
+<!--                    <v-btn text color="primary"  @click="$refs.dialog.save(menu.input)">OK</v-btn>-->
+<!--                </v-date-picker>-->
+<!--            </v-dialog>-->
 
-        <v-btn :disabled="isDisabled && (!date1 && !date2)" min-width="150" rounded color="primary" dark
+            <v-dialog
+                    ref="dialog"
+                    v-model="modal"
+                    v-for="(menu, index) in customMenu"
+                    :key="index"
+                    :return-value.sync="menu.input"
+                    persistent
+                    width="290px"
+            >
+                <template v-slot:activator="{ on }">
+                    <v-text-field
+                            v-model="menu.input"
+                            label="date"
+                            prepend-icon="event"
+                            :append-outer-icon="(index === customMenu.length - 1) ? 'mdi-plus-thick' : 'mdi-close-circle-outline'"
+                             @click:append-outer="manageInput(index)"
+                            v-on="on"
+                    ></v-text-field>
+                </template>
+                <v-date-picker v-model="menu.input" scrollable range>
+                    <v-spacer></v-spacer>
+                    <v-btn text color="primary" @click="modal = false">Cancel</v-btn>
+                    <v-btn text color="primary" @click="$refs.dialog[index].save(menu.input)">OK</v-btn>
+                </v-date-picker>
+            </v-dialog>
+        </div>
+
+        <v-btn v-if="!switchType" :disabled="isDisabled && (!date1 && !date2)" min-width="150" rounded
+               color="primary" dark
                @click="upload()">
             Submit
         </v-btn>
@@ -50,16 +112,21 @@
 </template>
 
 <script>
-    export default {
+  export default {
     name: "ControlsMean",
     data: () => ({
       date1: null,
       date2: null,
       menu1: false,
+      modal: false,
       menu2: false,
       period: null,
+      customMenu: [
+        {isActive: false, input: []}
+      ],
+      switchType: false,
       isDisabled: true,
-      text: 'Enter a date range to compare with the data for the entire period.'
+      text: 'Enter a date range or select custom dates to compare with the data for the entire period.'
     }),
     computed: {
       getPreloader() {
@@ -67,6 +134,19 @@
       }
     },
     methods: {
+      save(time) {
+        this.$refs.dialog.save(time)
+      },
+      manageInput(index) {
+        if (index ===  this.customMenu.length -1) {
+          this.customMenu.push({
+            isActive: false,
+            input: []
+          });
+        } else {
+          this.customMenu.splice(index, 1);
+        }
+      },
       changeInput() {
         if (this.date1 >= this.date2) {
           this.date1 = null;
@@ -87,7 +167,7 @@
         const end = lastDate.toISOString().substr(0, 10);
         const date = new Date();
         const startYear = new Date((date.setFullYear(date.getFullYear() - 2))).toISOString().substr(0, 10);
-        const endYear =  new Date().toISOString().substr(0, 10);
+        const endYear = new Date().toISOString().substr(0, 10);
         const dates = [];
         dates.push({start, end});
         this.$store.commit("setInfoSleep", null);
@@ -100,3 +180,8 @@
     },
   }
 </script>
+<style>
+    .v-icon.mdi-plus-thick {
+        color: #356859 !important;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
-import {store} from './store/index'
+import {store} from './store/index';
+import 'material-design-icons-iconfont';
 
 Vue.config.productionTip = false;
 
