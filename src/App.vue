919001814c9b21b795e29eaeb259183c9a68c42e
mirexdoors <mirex.doors@mail.ru>
2021-01-19 02:27:46 +0700

    fix firestore handlers

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
     <v-app class="mx-lg-4">
         <div
-            v-if="!globalPreloader"
-            :class="!isAuth ? 'd-flex' : ''"
-            class="fill-height"
+                v-if="!globalPreloader"
+                :class="!isAuth ? 'd-flex' : ''"
+                class="fill-height"
         >
             <preloader
-                v-if="getPreloader"
-                class="mx-auto"
+                    v-if="getPreloader"
+                    class="mx-auto"
             />
 
             <v-toolbar
-                v-if="isAuth"
-                color="primary"
+                    v-if="isAuth"
+                    color="primary"
             >
                 <v-btn>
                     <a
-                        class=" text-decoration-none"
-                        target="_blank"
-                        href="https://vllesh.medium.com/healthboard-tech-user-manual-5d075d46c408">
+                            class=" text-decoration-none"
+                            target="_blank"
+                            href="https://vllesh.medium.com/healthboard-tech-user-manual-5d075d46c408">
                             <span class="text-decoration-underline">
                             READ USER MANUAL
                             </span>
@@ -28,59 +28,59 @@
                 <v-spacer></v-spacer>
                 <v-btn icon>
                     <v-icon
-                        settings
-                        class="secondary--text"
-                        @click.stop="isOpenSettings = !isOpenSettings">settings
+                            settings
+                            class="secondary--text"
+                            @click.stop="isOpenSettings = !isOpenSettings">settings
                     </v-icon>
                 </v-btn>
                 <v-btn icon>
                     <Logout class="align-content-end secondary--text"/>
                 </v-btn>
                 <v-dialog
-                    v-model="isOpenSettings"
-                    max-width="300"
+                        v-model="isOpenSettings"
+                        max-width="300"
                 >
                     <settings @closeSettings="isOpenSettings = false"/>
                 </v-dialog>
             </v-toolbar>
-
+            <ServiceEmails v-if="isAuth || _isService"/>
             <v-main :class="isAuth ? '' : 'align-center'">
                 <v-card-title
-                    v-if="!getPreloader"
-                    class="text-center justify-center py-6">
+                        v-if="!getPreloader"
+                        class="text-center justify-center py-6">
                     <h1 class="font-weight-bold display-3 primary--text text-break px-2">
                         Health Board
                     </h1>
                 </v-card-title>
                 <v-col
-                    v-if="isAuth"
-                    cols="12"
-                    lg="7"
-                    class="d-flex flex-column mx-auto px-0 px-md-12 py-0"
+                        v-if="isAuth"
+                        cols="12"
+                        lg="7"
+                        class="d-flex flex-column mx-auto px-0 px-md-12 py-0"
                 >
                     <Tabs/>
                     <Data/>
                 </v-col>
                 <Authorization
-                    v-else
-                    class="mx-auto"
+                        v-else
+                        class="mx-auto"
                 />
             </v-main>
             <v-footer
-                light
-                absolute
-                inset
-                app
-                min-height="50"
-                class="d-flex justify-center"
+                    light
+                    absolute
+                    inset
+                    app
+                    min-height="50"
+                    class="d-flex justify-center"
             >
                 Feedback Email:
                 <a href="mailto:feedback@healthboard.tech" class="pl-2">feedback@healthboard.tech</a>
             </v-footer>
         </div>
         <div
-            v-else
-            class="my-auto"
+                v-else
+                class="my-auto"
         >
             <Preloader class="mx-auto"/>
         </div>
@@ -88,130 +88,142 @@
 </template>
 
 <script>
-import Cookies from "js-cookie";
-import axios from "axios";
+    import {mapGetters} from 'vuex';
+    import Cookies from "js-cookie";
+    import axios from "axios";
 
-import Logout from "./components/Logout";
-import Authorization from "./components/Authorization";
-import Data from "./components/main/Data";
-import Tabs from "./components/Tabs";
-import Settings from './components/settings';
-import Preloader from "./components/Preloader";
+    import Logout from "./components/Logout";
+    import Authorization from "./components/Authorization";
+    import Data from "./components/main/Data";
+    import Tabs from "./components/Tabs";
+    import Settings from './components/Settings';
+    import Preloader from "./components/Preloader";
+    import ServiceEmails from "./components/ServiceEmails";
 
-export default {
-    name: "App",
+    export default {
+        name: "App",
 
-    components: {
-        Authorization,
-        Data,
-        Logout,
-        Tabs,
-        Preloader,
-        Settings
-    },
-
-    data() {
-        return {
-            auth: false,
-            globalPreloader: true,
-            isOpenSettings: false,
-        };
-    },
-
-    computed: {
-        isAuth: function () {
-            return this.$store.state.Auth.token;
+        components: {
+            Authorization,
+            Data,
+            Logout,
+            Tabs,
+            Preloader,
+            Settings,
+            ServiceEmails,
         },
-        getPreloader: function () {
-            return this.$store.state.Data.preloader;
-        }
-    },
 
-    mounted() {
-        let token = this.getToken();
-        if (token) {
-            this.checksTokenEnDecay(token);
-        } else {
-            this.auth = false;
-            this.globalPreloader = false;
-        }
-    },
+        data() {
+            return {
+                auth: false,
+                globalPreloader: true,
+                isOpenSettings: false,
+            };
+        },
 
-    methods: {
-        getTokenInHref() {
-            let token;
-            const href = decodeURIComponent(location.href);
-            const token_lifetime = this.$store.state.Auth.token_lifetime;
+        computed: {
+            ...mapGetters(['userInfo']),
+            isAuth: function () {
+                return this.$store.state.Auth.token;
+            },
+            getPreloader: function () {
+                return this.$store.state.Data.preloader;
+            },
 
-            if (href.includes("access_token")) {
-                token = href.split("access_token=")[1].split("&")[0];
-                Cookies.set("token_oura", token, {expires: token_lifetime});
+            _isService() {
+                // if (this.userInfo?.email) {
+                //     return  this.userInfo.email === '4everladyjane@gmail.com';
+                // }
+                return false;
             }
-            const url = location.origin + "/";
-            history.pushState({page: 1}, document.title, url);
-            return token;
         },
 
-        getToken() {
-            let token = Cookies.get("token_oura") || null;
-            if (!token) token = this.getTokenInHref();
-            const url = location.origin + "/";
-            history.pushState({page: 1}, document.title, url);
-            return token;
+        mounted() {
+            let token = this.getToken();
+            if (token) {
+                this.checksTokenEnDecay(token);
+            } else {
+                this.auth = false;
+                this.globalPreloader = false;
+            }
         },
 
-        checksTokenEnDecay(token) {
-            axios
-                .get(`https://api.ouraring.com/v1/userinfo?access_token=${token}`)
-                .then(() => {
-                    if (!this.$store.state.Auth.token)
-                        this.$store.commit("saveToken", token);
-                    this.auth = true;
-                    this.globalPreloader = false;
-                })
-                .catch(e => {
-                    const codeError = new Error(e);
-                    if (codeError.message.includes(401)) {
-                        Cookies.remove("token_oura");
-                        this.auth = false;
+        methods: {
+            getTokenInHref() {
+                let token;
+                const href = decodeURIComponent(location.href);
+                const token_lifetime = this.$store.state.Auth.token_lifetime;
+
+                if (href.includes("access_token")) {
+                    token = href.split("access_token=")[1].split("&")[0];
+                    Cookies.set("token_oura", token, {expires: token_lifetime});
+                }
+                const url = location.origin + "/";
+                history.pushState({page: 1}, document.title, url);
+                return token;
+            },
+
+            getToken() {
+                let token = Cookies.get("token_oura") || null;
+                if (!token) token = this.getTokenInHref();
+                const url = location.origin + "/";
+                history.pushState({page: 1}, document.title, url);
+                return token;
+            },
+
+            checksTokenEnDecay(token) {
+                axios
+                    .get(`https://api.ouraring.com/v1/userinfo?access_token=${token}`)
+                    .then(response => {
+                        if (!this.$store.state.Auth.token)
+                            this.$store.commit("saveToken", token);
+                        this.auth = true;
                         this.globalPreloader = false;
-                    }
-                });
-        }
-    },
-};
+                        this.$store.dispatch('processEmail', response.data.email);
+                    })
+                    .catch(e => {
+                        const codeError = new Error(e);
+                        if (codeError.message.includes(401)) {
+                            Cookies.remove("token_oura");
+                            this.auth = false;
+                            this.globalPreloader = false;
+                        }
+                    });
+            }
+        },
+    };
 </script>
 
 <style>
-@media screen and (min-width: 991px) {
-    ::-webkit-scrollbar-track {
-        background: transparent
+    @media screen and (min-width: 991px) {
+        ::-webkit-scrollbar-track {
+            background: transparent
+        }
+
+        ::-webkit-scrollbar {
+            position: absolute;
+            width: 8px;
+            z-index: 1000;
+            background-color: transparent;
+        }
+
+        ::-webkit-scrollbar-thumb {
+            background-color: #ccc;
+            border-radius: 3px;
+            border: 1px solid #fff;
+        }
     }
 
-    ::-webkit-scrollbar {
-        position: absolute;
-        width: 8px;
-        z-index: 1000;
-        background-color: transparent;
+    .theme--dark.v-btn.v-btn--disabled:not(.v-btn--flat):not(.v-btn--text):not(.v-btn--outlined) {
+        background-color: #ccc !important;
     }
 
-    ::-webkit-scrollbar-thumb {
-        background-color: #ccc;
-        border-radius: 3px;
-        border: 1px solid #fff;
+    .white-input input {
+        color: #fff !important;
     }
-}
 
-.theme--dark.v-btn.v-btn--disabled:not(.v-btn--flat):not(.v-btn--text):not(.v-btn--outlined) {
-    background-color: #ccc !important;
-}
-
-.white-input input {
-    color: #fff !important;
-}
-
-.v-application .primary--text.white-input {
-    color: #ffffff;
-    caret-color: #fff;
-}
+    .v-application .primary--text.white-input {
+        color: #ffffff;
+        caret-color: #fff;
+    }
 </style>
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -2,75 +2,95 @@ import axios from "axios";
 import {db} from "../../db";
 
 const Auth = {
-  state: {
-    user: "user",
-    info: {
-      email: '',
-      country: '',
-      city: '',
-      timeZone: '',
-      gtmOffset: '',
-      dst: '',
+    state: {
+        user: "user",
+        info: {
+            email: '',
+            country: '',
+            city: '',
+            timeZone: '',
+            gtmOffset: '',
+            dst: '',
+        },
+        token: null,
+        parameteres: {
+            client_id: 'RKDF3YTKZSPGAPJO',
+            state: 'XXX',
+            response_type: 'token',
+            scope: 'email daily',
+            redirect_uri: location.origin + '/'
+        },
+        token_lifetime: 29,
     },
-    token: null,
-    parameteres: {
-      client_id: 'RKDF3YTKZSPGAPJO',
-      state: 'XXX',
-      response_type: 'token',
-      scope: 'email daily',
-      redirect_uri: location.origin + '/'
+    mutations: {
+        saveToken(state, payload) {
+            state.token = payload;
+        },
+        saveInfo(state, payload) {
+            state.info.email = payload.email;
+            state.info.country = payload.country;
+            state.info.city = payload.city;
+            state.info.timeZone = payload.timeZone;
+            state.info.gtmOffset = payload.gtmOffset;
+            state.info.dst = payload.dst;
+        },
+        updateTimeZone(state, payload) {
+            state.info.timeZone = payload;
+        },
+        updateGmt(state, payload) {
+            state.info.gtmOffset = payload;
+        },
+        updateDst(state, payload) {
+            state.info.dst = Number(payload);
+        }
     },
-    token_lifetime: 29,
-  },
-  mutations: {
-    saveToken(state, payload) {
-      state.token = payload;
-    },
-    saveInfo(state, payload) {
-      state.info.email = payload.email;
-      state.info.country = payload.country;
-      state.info.city = payload.city;
-      state.info.timeZone = payload.timeZone;
-      state.info.gtmOffset = payload.gtmOffset;
-      state.info.dst = payload.dst;
-    },
-    updateTimeZone(state, payload) {
-      state.info.timeZone = payload;
-    },
-    updateGmt(state, payload) {
-      state.info.gtmOffset = payload;
-    },
-    updateDst(state, payload) {
-      state.info.dst = Number(payload);
-    }
-  },
-  actions: {
-    getInfo({commit}) {
-      const token = this.state.Auth.token;
-      axios.get(`https://api.ouraring.com/v1/userinfo?access_token=${token}`)
-          .then(response => {
-            const info = {};
-            info.email = response.data.email;
-            db.collection('locations')
-                .doc(response.data.email)
-                .get()
-                .then(response => {
-                  if (response.exists) {
-                    const fireData = response.data();
-                    info.city= fireData.city;
-                    info.country = fireData.country;
-                    info.timeZone = fireData.timeZone;
-                    info.gtmOffset = fireData.gtmOffset;
-                    info.dst =  fireData.dst;
-                  }
-                  commit('saveInfo', info);
-                })
+    actions: {
+        processEmail(type, email) {
+            if (email) {
+                db.collection('email')
+                    .where('email', '==', email)
+                    .get()
+                    .then(response => {
+                        let isEmailExist = false;
+                        response.forEach(doc => {
+                            if (doc.data().email === email) isEmailExist = true;
+                        });
 
-              }
-          );
-    }
-  },
-  getters: {},
+                        if (!isEmailExist) {
+                            db.collection('email').add({email});
+                        }
+                    });
+            }
+        },
+
+        getInfo({commit}) {
+            const token = this.state.Auth.token;
+            axios.get(`https://api.ouraring.com/v1/userinfo?access_token=${token}`)
+                .then(response => {
+                        const info = {};
+                        info.email = response.data.email;
+
+                        db.collection('locations')
+                            .where('email', '==', response.data.email)
+                            .get()
+                            .then(response => {
+                                response.forEach(doc => {
+                                    const fireData = doc.data();
+                                    info.city = fireData.city;
+                                    info.country = fireData.country;
+                                    info.timeZone = fireData.timeZone;
+                                    info.gtmOffset = fireData.gtmOffset;
+                                    info.dst = fireData.dst;
+                                    commit('saveInfo', info);
+                                });
+                            });
+                    }
+                );
+        }
+    },
+    getters: {
+        userInfo: state => state.info,
+    },
 };
 
 export default Auth;
